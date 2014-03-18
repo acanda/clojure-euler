e425b08f7aedd692457f23317d0f7adb77d162b8
Philip Graf <git@acanda.ch>
2014-03-18 12:27:23 +0100

    add missing dependecy on org.clojure/contrib

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject euler "0.0.1-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"] [org.clojure/clojure-contrib "1.2.0"]]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"]
   :aot [euler.core]
   :main euler.core)
